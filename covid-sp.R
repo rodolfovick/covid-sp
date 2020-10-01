e589153b6695c1c60b1cc5a1cb3a8f6e8a404537
Rodolfo Vick <rodolfo.vick@furukawaelectric.com>
2020-10-01 17:33:29 -0300

    Add medical data.

--- covid-sp.R
+++ covid-sp.R
@@ -6,12 +6,16 @@ download.file("https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/d
 file <- read.csv("dados_covid_sp.csv", sep=";")
 cities <- unique(file$nome_munic)
 
+download.file("https://raw.githubusercontent.com/seade-R/dados-covid-sp/master/data/plano_sp_leitos_internacoes.csv", "plano_sp_leitos_internacoes.csv")
+hospital <- read.csv("plano_sp_leitos_internacoes.csv", sep=";")
+
 ui <- fluidPage(
   titlePanel("Covid-19 - Estado de São Paulo"),
   selectInput("city", "Selecione a cidade", choices = cities, selected = "São Paulo"),
   
   textOutput("title", container = h2),
   textOutput("general"),
+  textOutput("drsname"),
   textOutput("cases"),
   textOutput("death"),
   textOutput("rate"),
@@ -30,6 +34,9 @@ ui <- fluidPage(
   textOutput("z_death"),
   textOutput("d_death"),
   
+  plotOutput("p_uti"),
+  textOutput("t_ocup"),
+  
   hr(),
   p("2020 - Rodolfo Vick - rodolfo.vick at gmail.com"),
   br()
@@ -38,7 +45,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   # Create city data
   observe({
-  first <- TRUE
+    first <- TRUE
     for (i in 1:nrow(file)) {
       if (identical(file[i, "nome_munic"], input$city)){
         d = as.Date(paste("2020-", toString(file[i, "mes"]), "-", toString(file[i, "dia"]), sep=""))
@@ -46,6 +53,7 @@ server <- function(input, output, session) {
           cdata <- data.frame("data"=d ,"casos"=file[i, "casos"], "casos_novos"=file[i, "casos_novos"] ,"obitos"=file[i, "obitos"], "obitos_novos"=file[i, "obitos_novos"])
           pop <- file[i, "pop"]
           area <- file[i, "area"]
+          nome_drs <- file[i, "nome_drs"]
           first  <- FALSE
         }
         else {
@@ -62,6 +70,9 @@ server <- function(input, output, session) {
     })
     output$general <- renderText({
       paste("População:", pop, "hab. Área: ", area, "Km2. Densidade:", format(round(pop / area, 2), nsmall = 2), "hab/Km2.")
+    })
+    output$drsname <- renderText({
+      paste("Região:", nome_drs)
     })
     output$cases <- renderText({
       paste("Total de casos:", cdata[days, "casos"], ".")
@@ -178,6 +189,74 @@ server <- function(input, output, session) {
         paste("A média móvel é estável na última semana.")
       })
     }
+    
+    # Hospital data
+    first <- TRUE
+    for (i in 1:nrow(hospital)) {
+      if (identical("Grande São Paulo", nome_drs)) {
+        if (grepl("Município", hospital[i, "nome_drs"], fixed = TRUE)) {
+          d = as.Date(hospital[i, "datahora"], "%Y-%m-%d")
+          if (first) {
+            puti <- as.numeric(sub(",", ".", hospital[i, "pacientes_uti_mm7d"], fixed = TRUE))
+            tuti <- as.numeric(sub(",", ".", hospital[i, "total_covid_uti_mm7d"], fixed = TRUE))
+            for (j in 1:5) {
+              puti <- puti + as.numeric(sub(",", ".", hospital[i + j, "pacientes_uti_mm7d"], fixed = TRUE))
+              tuti <- tuti + as.numeric(sub(",", ".", hospital[i + j, "total_covid_uti_mm7d"], fixed = TRUE))
+            }
+            
+            mocup <- puti * 100 / tuti
+            
+            hdata <- data.frame("data"=d , "pacientes_uti_mm7d"=puti, "total_covid_uti_mm7d"=tuti, "media_ocupacao"=mocup)
+            first <- FALSE
+          }
+          else {
+            puti <- as.numeric(sub(",", ".", hospital[i, "pacientes_uti_mm7d"], fixed = TRUE))
+            tuti <- as.numeric(sub(",", ".", hospital[i, "total_covid_uti_mm7d"], fixed = TRUE))
+            for (j in 1:5) {
+              puti <- puti + as.numeric(sub(",", ".", hospital[i + j, "pacientes_uti_mm7d"], fixed = TRUE))
+              tuti <- tuti + as.numeric(sub(",", ".", hospital[i + j, "total_covid_uti_mm7d"], fixed = TRUE))
+            }
+            
+            mocup <- puti * 100 / tuti
+            
+            dt = list(d , puti, tuti, mocup)
+            hdata <- rbind(hdata, dt)
+          }
+        }
+      }
+      else {
+        if (grepl(nome_drs, hospital[i, "nome_drs"], fixed = TRUE)) {
+          d = as.Date(hospital[i, "datahora"], "%Y-%m-%d")
+          if (first) {
+            mocup <- as.numeric(sub(",", ".", hospital[i, "pacientes_uti_mm7d"], fixed = TRUE)) * 100 / as.numeric(sub(",", ".", hospital[i, "total_covid_uti_mm7d"], fixed = TRUE))
+            
+            hdata <- data.frame("data"=d , "pacientes_uti_mm7d"=as.numeric(sub(",", ".", hospital[i, "pacientes_uti_mm7d"], fixed = TRUE)), "total_covid_uti_mm7d"=as.numeric(sub(",", ".", hospital[i, "total_covid_uti_mm7d"], fixed = TRUE)), "media_ocupacao"=mocup)
+            first <- FALSE
+          }
+          else {
+            mocup <- as.numeric(sub(",", ".", hospital[i, "pacientes_uti_mm7d"], fixed = TRUE)) * 100 / as.numeric(sub(",", ".", hospital[i, "total_covid_uti_mm7d"], fixed = TRUE))
+            
+            dt = list(d , as.numeric(sub(",", ".", hospital[i, "pacientes_uti_mm7d"], fixed = TRUE)), as.numeric(sub(",", ".", hospital[i, "total_covid_uti_mm7d"], fixed = TRUE)), mocup)
+            hdata <- rbind(hdata, dt)
+          }
+        }
+      }
+    }
+    
+    hmax <- max(c(max(hdata$total_covid_uti_mm7d), max(hdata$pacientes_uti_mm7d)))
+    hmin <- min(c(min(hdata$total_covid_uti_mm7d), min(hdata$pacientes_uti_mm7d)))
+    ocupmax <- which.max(hdata$media_ocupacao)
+    
+    output$p_uti <- renderPlot({
+      plot(hdata$data, hdata$total_covid_uti_mm7d, type = "h", main = paste("Média móvel para 7 dias de ocupação de UTI\nRegião:", nome_drs), xlab = NA, ylab = NA, col="blue", ylim = c(hmin, hmax))
+      par(new=TRUE)
+      plot(hdata$data, hdata$pacientes_uti_mm7d, type = "h", xlab = NA, ylab = NA, col="red", ylim = c(hmin, hmax))
+      legend("topright", legend=c("Oferta de leitos", "Ocupação"), pch=15, col=c("blue", "red"))
+    })
+    
+    output$t_ocup <- renderText({
+      paste("A maior taxa média de ocupação de UTI foi de ", format(round(hdata[ocupmax, "media_ocupacao"], 2), 2), "% em", as.character(hdata[ocupmax, "data"], "%d/%m/%Y"), "na região", nome_drs, ".")
+    })
     
   })
 }
